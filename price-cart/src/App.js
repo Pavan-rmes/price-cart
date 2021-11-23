63fc9ba872a393e0e0e56bb97604b49cb37c7e3e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-23 07:48:45 +0530

    data-into the cards

--- price-cart/src/App.js
+++ price-cart/src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import Button from '@mui/material/Button';
 import {data} from "./data.js"
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
 function App() {
   return (
     <div className="App">
-      <Buyoptions key="free" />
-      <Buyoptions />
-      <Buyoptions />
+      <Buyoptions value="free" />
+      <Buyoptions value="plus" />
+      <Buyoptions value="pro" />
     </div>
   );
 }
 
-function Buyoptions({prop}){
+function Buyoptions({value}){
+  console.log(value)
   return(
     <div className="buy-card">
-      {data.free.map((value)=>console.log(value))}
+      {data[value].map((value,index)=>{
+        <div>
+          value[0]==true?<DoneIcon /> :<CloseIcon />
+          <p>{value[1]}</p>
+        </div>
+      })}
       <Button style={{width:"50px"}} variant="contained">Buy</Button>
     </div>
   )
--- price-cart/src/data.js
+++ price-cart/src/data.js
@@ -1,3 +1,28 @@
-const data = { free: ["Single User", "5GB Storage", "Unlimited Public Projects", "Community Acess", "Unlimited Private Projects", "Dedicated Phone Support", "Free Subdomain", "Montly Status Report"] };
+const data = { free: [
+        [true,"Single User"], 
+        [true,"5GB Storage"], 
+        [true,"Unlimited Public Projects"], 
+        [true,"Community Acess"], 
+        [false,"Unlimited Private Projects"], 
+        [false,"Dedicated Phone Support"], 
+        [false,"Free Subdomain"], 
+        [false,"Montly Status Report"]],
+    plus:[
+        [true,"5 Users"],
+        [true,"50GB Storage"],
+        [true,"Unlimited Public Projects"],
+        [true,"Community Access"],
+        [true,"Unlimited Private Projects"],
+        [true,"Dedicated Phone Support"],
+        [true,"Free Subdomain"],
+        [true,"Monthly Status Reports"]],
+    pro:[
+        [true,"Unlimited Users"],
+        [true,"150GB Storage"],
+        [true,"Community Access"],
+        [true,"Unlimited Private Projects"],
+        [true,"Dedicated Phone Support"],
+        [true,"Unlimited Free Subdomains"],
+        [true,"Monthly Status Reports"]] };
 
 export {data}
